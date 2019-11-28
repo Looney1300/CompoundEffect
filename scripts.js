cdfb7d04bdaf9ff63277f25ad417543aa281166e
Landon Henrie <Looney1300@gmail.com>
2019-11-28 13:53:39 -0700

    Working first draft.

---
+++ scripts.js
@@ -0,0 +1,59 @@
+var pageStart = (function(){
+    let money = {
+        reset: function (){
+            money.principal = Number(document.getElementById('principal').value);
+            money.originalPrincipal = money.principal;
+            money.interestRate = Number(document.getElementById('interestRate').value)/100;
+            money.nCompounds = Number(document.getElementById('nCompounds').value);
+            money.addToBalance = Number(document.getElementById('addToBalance').value);
+            money.addToFrequency = Number(document.getElementById('addToFrequency').value);
+            money.moneyGoal = Number(document.getElementById('moneyGoal').value);
+            money.addedBalanceTotal = 0;
+        }
+    }
+
+    function compound(){
+        money.reset();
+        document.getElementById('log').innerHTML = '<tr><th>Year</th><th>Balance</th><th>Interest</th><th>You Added</th></tr>';
+
+        let year = 0;
+        while (money.principal < money.moneyGoal){
+            // Have to loop in order to add to the balance periodically.
+            addRow(year);
+            let n=0;
+            for (let i=0; i<money.nCompounds; ++i){
+                while (i >= money.nCompounds/money.addToFrequency * n){
+                    n += 1;
+                    money.principal += money.addToBalance;
+                    money.addedBalanceTotal += money.addToBalance;
+                }
+                money.principal = money.principal*(1 + (money.interestRate/money.nCompounds));
+            }
+            ++year;
+        }
+        // Add final year after reaching goal.
+        addRow(year);
+        // Say how many years it will take.
+        document.getElementById('yearsToHitGoal').innerText = 
+            `It will take ${year} years to have $${money.moneyGoal} by starting with $${money.originalPrincipal} with ${(money.interestRate*100).toFixed(2)}% APY (compounding ${money.nCompounds}/yr) contributing $${money.addToBalance} every ${(365/money.addToFrequency).toFixed(2)} days.`;
+
+    }
+
+    function addRow(i){
+        document.getElementById('log').innerHTML += 
+            `<tr>
+                <td>${i}</td><td>$${money.principal.toFixed(2)}</td><td>$${(money.principal - money.addedBalanceTotal - money.originalPrincipal).toFixed(2)}</td><td>$${(money.addedBalanceTotal + money.originalPrincipal).toFixed(2)}</td>
+            </tr>`;
+    }
+
+    let inputs = document.getElementsByClassName('form-control');
+    for(let i=0; i<inputs.length; ++i){
+        inputs[i].onchange = compound;
+    }
+
+    function pageStart(){
+        compound();
+    }
+
+    return pageStart;
+})();
